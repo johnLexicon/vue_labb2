040786725ff9d26e28b90c4bc15befda5678d419
john lundgren <john.lundgren.lexicon@gmail.com>
2020-11-24 18:55:45 +0100

    Code formatting.

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,42 +1,59 @@
 <template>
   <!-- Default form login -->
-<form class="text-center border border-light p-5" @submit.prevent="logIn">
-
+  <form class="text-center border border-light p-5" @submit.prevent="logIn">
     <p class="h4 mb-4">Sign in</p>
 
     <!-- Name -->
-    <input v-model="name" type="text" id="defaultLoginFormName" class="form-control mb-4" placeholder="name">
+    <input
+      v-model="name"
+      type="text"
+      id="defaultLoginFormName"
+      class="form-control mb-4"
+      placeholder="name"
+    />
 
     <!-- Password -->
-    <input v-model="password" type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
+    <input
+      v-model="password"
+      type="password"
+      id="defaultLoginFormPassword"
+      class="form-control mb-4"
+      placeholder="Password"
+    />
 
     <!-- Sign in button -->
-    <button :disabled="loggedIn" class="btn btn-info btn-block my-4" type="submit">Sign in</button>
-
-</form>
-<!-- Default form login -->
+    <button
+      :disabled="loggedIn"
+      class="btn btn-info btn-block my-4"
+      type="submit"
+    >
+      Sign in
+    </button>
+  </form>
+  <!-- Default form login -->
 </template>
 
 <script>
 export default {
-    props:['loggedIn'],
-    data(){
-        return {
-            name:'',
-            password:''
-        }
+  props: ["loggedIn"],
+  data() {
+    return {
+      name: "",
+      password: "",
+    };
+  },
+  methods: {
+    logIn() {
+      if (this.loggedIn) {
+        alert("You have to log out first!!!!");
+        return;
+      }
+      const user = { name: this.name, password: this.password };
+      this.$emit("logInUser", user);
+      (this.name = ""), (this.password = "");
     },
-    methods: {
-        logIn(){
-            if(this.loggedIn){
-                alert('You have to log out first!!!!');
-                return;
-            }
-            const user = {name: this.name, password: this.password};
-            this.$emit('logInUser', user);
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
