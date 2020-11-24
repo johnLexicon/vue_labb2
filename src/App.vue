a8ea37ea2fad47792ec0031c41f84f40a8ba60c4
john lundgren <john.lundgren.lexicon@gmail.com>
2020-11-24 16:56:06 +0100

    Added AppForm component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <AppForm/>
   </div>
 </template>
 
 <script>
-
+import Form from './components/Form';
 export default {
   name: 'App',
   components: {
+    AppForm: Form
   }
 }
 </script>
 
 <style>
-
+  *{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
 </style>
