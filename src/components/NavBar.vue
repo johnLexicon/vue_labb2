65da96de510602e0425733ae1f95c926c951f7ac
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-03 08:10:27 +0200

    Added Lexicon logo.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,41 +2,41 @@
   <!-- Just an image -->
   <nav class="navbar navbar-dark primary-color">
     <a class="navbar-brand" href="#">
-      <img
-        src="https://mdbootstrap.com/img/logo/mdb-transparent.png"
-        height="30"
-        alt="mdb logo"
-      />
+      <img src="@/assets/img/lexicon.jpg" height="30" alt="Lexicon logo" />
     </a>
 
     <div v-if="loggedIn === false">
-        <span class="navbar-text white-text">
-          Please Log In
-        </span>
+      <span class="navbar-text white-text"> Please Log In </span>
     </div>
     <div v-else>
-        <span class="navbar-text white-text">
-            Logged in as: {{ user.name }}
-        </span>
-        <button @click="logOut" class="btn btn-sm btn-outline-white" type="button">Log out</button>
+      <span class="navbar-text white-text">
+        Logged in as: {{ user.name }}
+      </span>
+      <button
+        @click="logOut"
+        class="btn btn-sm btn-outline-white"
+        type="button"
+      >
+        Log out
+      </button>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-    props: ['loggedIn', 'user'],
-    methods: {
-        logOut(){
-            this.$emit('logOutUser');
-        }
-    }
+  props: ["loggedIn", "user"],
+  methods: {
+    logOut() {
+      this.$emit("logOutUser");
+    },
+  },
 };
 </script>
 
 <style scoped>
-    div span{
-        font-weight: 400;
-        margin-right: 1rem;
-    }
+div span {
+  font-weight: 400;
+  margin-right: 1rem;
+}
 </style>
